c3514972bb06787c6515cc91dd94833e3c3eefb9
alexeykopievskiy <alexeykopievskiy@gmail.com>
2016-10-29 00:35:03 +0300

    clean app

--- app/index.js
+++ app/index.js
@@ -4,7 +4,7 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router, hashHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
-import routes from './routes';
+import App from './containers/App';
 import configureStore from './store/configureStore';
 import './app.global.css';
 
@@ -13,7 +13,7 @@ const history = syncHistoryWithStore(hashHistory, store);
 
 render(
   <Provider store={store}>
-    <Router history={history} routes={routes} />
+    <App />
   </Provider>,
   document.getElementById('root')
 );
