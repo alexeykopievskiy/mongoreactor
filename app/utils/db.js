6ef8e6c3aa301ca692cae08b76cff35a2e31c4ea
alexeykopievskiy <alexeykopievskiy@gmail.com>
2016-10-29 01:25:22 +0300

    create DB util for connection

---
+++ app/utils/db.js
@@ -0,0 +1,9 @@
+export default class DB {
+
+  static connect(params){
+    const connectUrl = `mongodb://${params.server}:${params.port}/${params.database}`
+    return this.connectDB(connectUrl, params.database)
+  }
+
+  static connectDB(connectUrl, dbName)
+}
