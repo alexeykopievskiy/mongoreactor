79893bb1609dae44cea40656a20454e4aa01b232
alexeykopievskiy <alexeykopievskiy@gmail.com>
2016-10-29 11:02:25 +0300

    clean db.js

--- app/utils/db.js
+++ app/utils/db.js
@@ -1,22 +1,16 @@
-/*const MongoClient = require('mongodb').MongoClient;
-const Server = require('mongodb').Server*/
-
 const MongoClient = require('electron').remote.require('mongodb').MongoClient;
-const Server = require('electron').remote.require('mongodb').Server;
-
-console.log(MongoClient, Server, '1-2');
 
 
 export default class DB {
 
   constructor(){
     this.MongoConnect = null
+    this.MongoError = null
     this.serverAddr = null
     this.serverPort = null
   }
 
   static connect(params){
-    console.log(params, 'para');
     this.serverAddr = params.server_url;
     this.serverPort = params.server_port;
 
@@ -24,15 +18,10 @@ export default class DB {
   }
 
   static connectDB(server_url, server_port){
-    console.log(server_url, server_port, 'connectDb');
-    this.MongoConnect = MongoClient.connect('mongodb://localhost:27017/test', (error, db) => {
-      console.log(error, 'err');
-      console.log(db, 'db');
+    MongoClient.connect(`mongodb://${server_url}:${server_port}/test`, (error, database) => {
+      this.MongoError = error;
+      this.MongoConnect = database;
     })
-    /*this.MongoConnect.connect().then(response => {
-      console.log(response, 'response');
-    })*/
-    console.log(this.MongoConnect.collection(), 'log');
 
   }
 
