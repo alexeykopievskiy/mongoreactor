8b56f79081d991c649b76efe4ca0e3a641882d3d
alexeykopievskiy <alexeykopievskiy@gmail.com>
2016-10-29 11:53:00 +0300

    test gitAllDbs without login

--- app/actions/connect.js
+++ app/actions/connect.js
@@ -2,9 +2,11 @@ import * as constants from '../constants/connect.const'
 import DB from '../utils/db'
 
 export function connectDB(){
-  
+
   // test case
-  DB.connect({server_url: 'localhost', server_port: 27017});
+  DB.connect({server_url: 'localhost', server_port: 27017}).then(() => {
+    DB.listAllDatabase()
+  });
   return {
     type: constants.CONNECT
   }
--- app/utils/db.js
+++ app/utils/db.js
@@ -14,18 +14,30 @@ export default class DB {
     this.serverAddr = params.server_url;
     this.serverPort = params.server_port;
 
-    this.connectDB(this.serverAddr, this.serverPort);
+    return this.connectDB(this.serverAddr, this.serverPort);
   }
 
   static connectDB(server_url, server_port){
-    MongoClient.connect(`mongodb://${server_url}:${server_port}/test`, (error, database) => {
-      this.MongoError = error;
-      this.MongoConnect = database;
+    return new Promise((resolve, reject) => {
+      MongoClient.connect(`mongodb://${server_url}:${server_port}/test`, (error, database) => {
+        this.MongoError = error;
+        this.MongoConnect = database;
+        console.log(this.MongoConnect, 'ddd');
+        resolve(database)
+      })
     })
-
   }
 
   static disconnectDB(){
+    this.MongoConnect.close()
+  }
 
+  static listAllDatabase(){
+    console.log(this.MongoConnect, 'mongoco');
+    const admin = this.MongoConnect.admin()
+    admin.listDatabases((error, databases) => {
+      console.log(error, 'err');
+      console.log(databases, 'dbs');
+    })
   }
 }
